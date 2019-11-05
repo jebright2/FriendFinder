db9b36ce27bc64064ca8e500bdb692aae52847d1
Da Vinci <jeb2je@gmail.com>
2019-11-05 14:44:00 -0500

    added questions for survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,12 +3,12 @@ var surveyData = require("../data/friends");
 
 module.exports = function(app) {
 //For display of all possible friends
-app.get("api/friends", function(req, res) {
+app.get("/api/friends", function(req, res) {
     res.json(surveyData);
     })
 
 //Post from incoming survey results
-app.post("api/friends", function(req, res) {
+app.post("/api/friends", function(req, res) {
     res.json(surveyData);
     })
 };
