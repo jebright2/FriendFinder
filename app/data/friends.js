ac94a13b8a5cfea01393f69e77805291eb71ff2a
Da Vinci <jeb2je@gmail.com>
2019-11-06 12:50:44 -0500

    submission button and functions work based on console; unable to post

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 
 var friendsArr = [
     {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Dwayne",
+    "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5b3a90cba7ea4353dd3f9804%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D186%26cropX2%3D3092%26cropY1%3D449%26cropY2%3D3354&imgrefurl=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdwayne-johnson%2F&docid=ENJmIYuMKkXWOM&tbnid=uNGvOh5-5_fvYM%3A&vet=10ahUKEwi53Yzc_dXlAhXyYt8KHcdsD7oQMwh5KAIwAg..i&w=416&h=416&bih=578&biw=1280&q=dwayne%20johnson&ved=0ahUKEwi53Yzc_dXlAhXyYt8KHcdsD7oQMwh5KAIwAg&iact=mrc&uact=8",
     "scores":[
         5,
         1,
